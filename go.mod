391ec48c6233f823a48e473bc8ff289e1b93826b
sunfeilong <sunfeilong@eglsgame.com>
2020-09-27 15:41:37 +0800

    日志

--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/dgraph-io/dgo/v200 v200.0.0-20200916081436-9ff368ad829a
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.4.0
+	go.uber.org/zap v1.16.0
 	golang.org/x/net v0.0.0-20200923182212-328152dc79b1 // indirect
 	golang.org/x/sys v0.0.0-20200923182605-d9f96fdee20d // indirect
 	golang.org/x/text v0.3.3 // indirect
 	google.golang.org/genproto v0.0.0-20200923140941-5646d36feee1 // indirect
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,13 +3,15 @@ package client
 import (
     "context"
     "encoding/json"
-    "fmt"
     "github.com/dgraph-io/dgo/v200"
     "github.com/dgraph-io/dgo/v200/protos/api"
+    "github.com/xiaotian/dgraph-person/pkg/d-log"
     "github.com/xiaotian/dgraph-person/pkg/data"
     "google.golang.org/grpc"
     "strings"
 )
+
+var logger = d_log.New()
 
 type Client struct {
     ip   string
@@ -24,11 +26,13 @@ type DecodePerson struct {
 func (c *Client) Connect(ip string, port int) {
     c.ip = ip
     c.port = port
-    dial, err := grpc.Dial("10.0.8.36:19080", grpc.WithInsecure())
+    logger.Infow("connect to DGraph server start", "ip", ip, "port", port)
+    dial, err := grpc.Dial(ip+":"+string(port), grpc.WithInsecure())
     if err != nil {
-        panic(err)
+        logger.Panicw("connect to DGraph server error", "ip", ip, "port", port, "err", err.Error())
     }
     c.dg = dgo.NewDgraphClient(api.NewDgraphClient(dial))
+    logger.Infow("connect to DGraph server success", "ip", ip, "port", port)
 }
 
 func (c *Client) AddSchema(schema string) (bool, error) {
@@ -36,14 +40,19 @@ func (c *Client) AddSchema(schema string) (bool, error) {
         Schema:          schema,
         RunInBackground: false,
     }
+    logger.Infow("AddSchema start", "schema", schema)
     err := c.dg.Alter(context.Background(), op)
     if err != nil {
+        logger.Errorw("AddSchema error", "schema", schema, "err", err.Error())
         return false, err
     }
+    logger.Infow("AddSchema start success", "schema", schema)
     return true, nil
 }
 
 func (c *Client) AddPerson(name string, phone string) (bool, error) {
+
+    logger.Infow("AddPerson start", "name", name, "phone", phone)
 
     addData := data.Person{
         Uid:      "_:person",
@@ -52,52 +61,77 @@ func (c *Client) AddPerson(name string, phone string) (bool, error) {
         DType:    []string{"Person"},
         NodeType: "Person",
     }
-
     jsonData, err := json.Marshal(addData)
     if nil != err {
-        panic(err)
+        logger.Errorw("AddPerson json marshall error", "err", err)
+        return false, err
     }
+
     mu := &api.Mutation{
         CommitNow: true,
         SetJson:   jsonData,
     }
 
-    response, err := c.dg.NewTxn().Mutate(context.Background(), mu)
+    ctx := context.Background()
+    txn := c.dg.NewTxn()
+    response, err := txn.Mutate(ctx, mu)
+    defer txn.Discard(ctx)
+
     if err != nil {
+        logger.Errorw("AddPerson mutation error", "name", name, "phone", phone, "err", err)
         return false, err
     }
+
+    logger.Infow("AddPerson mutation response", "name", name, "phone", phone, "response", string(response.Json))
     if response.Json != nil {
         return false, nil
     }
+    logger.Infow("AddPerson mutation success", "name", name, "phone", phone, "response", string(response.Json))
     return true, nil
 }
 
 func (c *Client) GetPersonByUid(uid string) *data.Person {
+    logger.Infow("GetPersonByUid start", "uid", uid)
+
     txn := c.dg.NewTxn()
     ctx := context.Background()
+    defer txn.Discard(ctx)
     queryStr := strings.Replace(data.QueryByUid, "$phone", uid, 1)
+    logger.Infow("GetPersonByUid start", "uid", uid, "queryStr", queryStr)
+
     res, err := txn.Query(ctx, queryStr)
     if err != nil {
-        panic(err)
+        logger.Panicw("GetPersonByUid execute query error", "uid", uid, "queryStr", queryStr, "err", err)
     }
+
     decode := DecodePerson{}
     err = json.Unmarshal(res.Json, &decode)
+    if err != nil {
+        logger.Panicw("GetPersonByUid unmarshal json  error", "uid", uid, "err", err, "res", string(res.Json))
+    }
+
     if len(decode.P) == 0 {
+        logger.Infow("GetPersonByUid node not exists", "uid", uid)
         return nil
-    } else if len(decode.P) == 1 {
-        return &decode.P[0]
     } else {
-        panic("GetPersonByUid 数据存在错误")
+        logger.Infow("GetPersonByUid node exists", "uid", uid, "node", decode.P[0])
+        return &decode.P[0]
     }
 }
 
 func (c *Client) GetPersonByEdge(edgeName string, value string) *data.Person {
+    logger.Infow("GetPersonByEdge start", "edgeName", edgeName, "value", value)
+
     txn := c.dg.NewTxn()
     ctx := context.Background()
+    defer txn.Discard(ctx)
+
     queryStr := strings.Replace(strings.Replace(data.QueryByEdge, "$edgeName", edgeName, 1), "$phone", value, 1)
+    logger.Infow("GetPersonByEdge queryStr", "edgeName", edgeName, "value", value, "queryStr", queryStr)
+
     res, err := txn.Query(ctx, queryStr)
     if err != nil {
-        panic(err)
+        logger.Panicw("GetPersonByEdge query error", "edgeName", edgeName, "value", value, "queryStr", queryStr, "err", err)
     }
     decode := DecodePerson{}
     err = json.Unmarshal(res.Json, &decode)
@@ -111,7 +145,10 @@ func (c *Client) GetPersonByEdge(edgeName string, value string) *data.Person {
 }
 
 func (c *Client) DeleteByUid(uidList ...string) bool {
+    logger.Infow("DeleteByUid start", "uidList", uidList)
+
     if len(uidList) == 0 {
+        logger.Warnw("DeleteByUid uidList is empty", "uidList", uidList)
         return true
     }
 
@@ -120,20 +157,20 @@ func (c *Client) DeleteByUid(uidList ...string) bool {
         mutations[i] = strings.Replace(data.DeleteNode, "$uid", uid, 1)
     }
     deleteMutationList := strings.Join(mutations, "\n")
-
-    fmt.Println(deleteMutationList)
+    logger.Warnw("DeleteByUid deleteMutationList", "deleteMutationList", deleteMutationList)
 
     mu := &api.Mutation{
         CommitNow: true,
         DelNquads: []byte(deleteMutationList),
     }
-
     txn := c.dg.NewTxn()
     ctx := context.Background()
     mutate, err := txn.Mutate(ctx, mu)
     if nil != err {
-        panic(err)
+        logger.Panicw("DeleteByUid mutation error", "err", err)
     }
+
+    logger.Info("DeleteByUid response", "response", string(mutate.Json))
     if mutate.Json == nil {
         return true
     }
