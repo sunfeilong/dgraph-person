d4fa25700004f8fbdc6ff417a7b61a1ac3389daf
sunfeilong <sunfeilong@eglsgame.com>
2020-09-28 10:52:04 +0800

    phone 加 term 索引

--- pkg/data/schema.go
+++ pkg/data/schema.go
@@ -4,12 +4,12 @@ const Schema string = `
   type Person{
     node.type # 用于表示节点的类型 (可能不需要)
     name      # 姓名
-    phone     # 电话号码 
+    phone     # 电话号码
     friend    # 朋友
   }
 
   name: string @index(term, exact) .
-  phone: string  @index(exact) .
+  phone: string  @index(exact, term) .
   friend: [uid] .
   node.type: string  @index(exact) .
   `
